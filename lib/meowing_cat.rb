7d9e84f06336e9d6612769de47b03ac2843e50fb
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-18 16:04:57 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,8 +1,7 @@
-## code your solution here. 
-class Cat 
+## code your solution here.
+class Cat
   attr_accessor :name
   def meow
     puts "Me-Fucking-ow!, I am Cat, do what I command!"
   end
 end
-
